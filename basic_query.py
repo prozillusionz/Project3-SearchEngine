62ddc385d9b55844fd19e9189a4b619aeb3bcd2e
prozillusionz <wendtk@uci.edu>
2024-03-05 21:07:08 -0800

    Added comments

--- basic_query.py
+++ basic_query.py
@@ -8,25 +8,34 @@ from nltk.stem import WordNetLemmatizer
 from bs4 import BeautifulSoup
 import pickle
 
+# Getting associated docuemtns related to a search query
 def get_documents(query_term):
     words = query_term.split()
     with open('data.pickle', 'rb') as f:
         existing_data = pickle.load(f)
+    # Check if the first word of the search query exists in the data and is a list
     if isinstance(existing_data[words[0]], list):
         documents = set()
+        # Each dictionary in the list of the first word
         for doc_dict in existing_data[words[0]]:
             if isinstance(doc_dict, dict):
+                #Add the keys (document IDs) to the set of documents
                 documents |= set(doc_dict.keys())
     else:
         #print(f"existing_data[words[0]] is not a list of dictionaries")
-        return []
+        return [] # Empty List
+    # Remaining words in the query term
     for word in words[1:]:
+        # Check if the word of the search query exists in the data and is a list
         if word in existing_data and isinstance(existing_data[word], list):
             word_documents = set()
+            # Each dictionary in the list of the current word
             for doc_dict in existing_data[word]:
                 if isinstance(doc_dict, dict):
+                    # Add the keys (document IDs) to the set of documents of the current word
                     word_documents |= set(doc_dict.keys())
-            documents &= word_documents  # intersection of sets
+            # Update the set of documents
+            documents &= word_documents # Intersection of sets
         else:
             #print(f"Word '{word}' not found in existing data.")
             return []  # return an empty list if the word is not found to prevent keyerror
